8691599b6ddf022909770eebe5c2caa586ad395b
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-10-20 12:04:39 +0530

    Add heper function to get payment method label

--- includes/functions.php
+++ includes/functions.php
@@ -85,3 +85,15 @@ function give_iats_get_server_name() {
 
 	return $server;
 }
+
+
+/**
+ * Get payment method label.
+ *
+ * @return string
+ */
+function give_iats_get_payment_method_label() {
+	$give_settings = give_get_settings();
+
+	return ( empty( $give_settings['iats_payment_method_label'] ) ? __( 'Credit Card', 'give-iatspayments' ) : $give_settings['iats_payment_method_label'] );
+}
