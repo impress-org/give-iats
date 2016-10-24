09d0db2d9b6b457a4d490062761be05260d4e1aa
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-10-24 13:05:12 +0530

    Refactor code

--- plugin.php
+++ plugin.php
@@ -43,8 +43,51 @@
 /**
  * Class Give_iATS_Gateway
  */
-class Give_iATS_Gateway {
-	function __construct() {
+final class Give_iATS_Gateway {
+
+	/**
+	 * Instance.
+	 *
+	 * @since  1.0
+	 * @access static
+	 * @var Give_iATS_Gateway $instance
+	 */
+	static private $instance;
+
+
+	/**
+	 * Singleton pattern.
+	 *
+	 * Give_iATS_Gateway constructor.
+	 */
+	private function __construct() {
+	}
+
+
+	/**
+	 * Get instance.
+	 *
+	 * @since  1.0
+	 * @access static
+	 * @return Give_iATS_Gateway
+	 */
+	static function get_instance() {
+		if ( null === self::$instance ) {
+			self::$instance = new static();
+		}
+
+		return self::$instance;
+	}
+
+
+	/**
+	 * Load files.
+	 *
+	 * @since  1.0
+	 * @access public
+	 * @return Give_iATS_Gateway
+	 */
+	public function load_files() {
 		// iATS payment gateways core.
 		require_once 'includes/lib/iATSPayments/iATS.php';
 
@@ -68,20 +111,46 @@ class Give_iATS_Gateway {
 			require_once 'includes/admin/actions.php';
 		}
 
+		return self::$instance;
+	}
+
+
+	/**
+	 * Setup hooks.
+	 *
+	 * @since  1.0
+	 * @access public
+	 * @return Give_iATS_Gateway
+	 */
+	function setup_hooks() {
 		// Load scripts and style.
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+
+		return self::$instance;
 	}
 
 	/**
 	 * Enqueue scripts.
 	 *
+	 * @since  1.0
+	 * @access public
+	 *
 	 * @param $hook
 	 */
-	function enqueue_scripts( $hook ) {
-		if( 'gateways' === give_get_current_setting_tab() && 'iatspayments' === give_get_current_setting_section() ) {
-			wp_enqueue_script( 'iats-admin-settings', plugins_url( '/assets/js/admin/admin-settings.js', __FILE__), array('jquery') );
+	public function enqueue_scripts( $hook ) {
+		if ( 'gateways' === give_get_current_setting_tab() && 'iatspayments' === give_get_current_setting_section() ) {
+			wp_enqueue_script( 'iats-admin-settings', plugins_url( '/assets/js/admin/admin-settings.js', __FILE__ ), array( 'jquery' ) );
 		}
 	}
 }
 
-new Give_iATS_Gateway();
+// Initiate plugin.
+function give_iats_plugin_init() {
+	if ( class_exists( 'Give' ) ) {
+		Give_iATS_Gateway::get_instance()
+		                 ->load_files()
+		                 ->setup_hooks();
+	}
+}
+
+add_action( 'plugins_loaded', 'give_iats_plugin_init' );
