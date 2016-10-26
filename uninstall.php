df01af0d52796270bae94258db662dd09999348a
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-10-26 21:34:00 +0530

    add uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,21 @@
+<?php
+// if uninstall.php is not called by WordPress, die
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die;
+}
+
+// Get give plugin settings.
+$give_settings = give_get_settings();
+
+
+// Remove plugin settings.
+unset( $give_settings['iats_sandbox_testing'] );
+unset( $give_settings['iats_payment_method_label'] );
+unset( $give_settings['iats_sandbox_agent_code'] );
+unset( $give_settings['iats_sandbox_agent_password'] );
+unset( $give_settings['iats_live_agent_code'] );
+unset( $give_settings['iats_live_agent_password'] );
+
+
+// Update option.
+update_option( 'give_settings', $give_settings );
