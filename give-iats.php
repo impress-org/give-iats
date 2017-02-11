e371d3cfaabdd1525f5441d3920af497e4458f46
Devin Walker <devin@wordimpress.com>
2017-02-10 16:51:24 -0800

    Removed unnecessary copy

--- give-iats.php
+++ give-iats.php
@@ -9,34 +9,6 @@
  * Text Domain: give-iatspayments
  * Domain Path: /languages
  * GitHub Plugin URI: https://github.com/WordImpress/Give-iATS
- *
- * Give - iATS Payments is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * Give - iATS Payments is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Give - iATS Payments. If not, see <https://www.gnu.org/licenses/>.
- *
- * A Tribute to Open Source:
- *
- * "Open source software is software that can be freely used, changed, and shared (in modified or unmodified form) by anyone. Open
- * source software is made by many people, and distributed under licenses that comply with the Open Source Definition."
- *
- * -- The Open Source Initiative
- *
- * Give - iATS Payments is a tribute to the spirit and philosophy of Open Source. We at WordImpress gladly embrace the Open Source philosophy both
- * in how Give - iATS Payments itself was developed, and how we hope to see others build more from our code base.
- *
- * Give - iATS Payments would not have been possible without the tireless efforts of WordPress and the surrounding Open Source projects and their talented developers. Thank you all for your contribution to WordPress.
- *
- * - The WordImpress Team
- *
  */
 
 
