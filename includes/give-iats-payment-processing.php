bbb4c83d42229946ffa503f66ba355a14267bc61
Devin Walker <devin@wordimpress.com>
2017-03-01 17:18:25 -0800

    Updated payment args New give_iats_set_error()

--- includes/give-iats-payment-processing.php
+++ includes/give-iats-payment-processing.php
@@ -7,6 +7,7 @@
  * @param $donation_data
  */
 function give_process_iats_payment( $donation_data ) {
+
 	if ( ! wp_verify_nonce( $donation_data['gateway_nonce'], 'give-gateway' ) ) {
 		wp_die( esc_html__( 'Nonce verification has failed.', 'give-iats' ), esc_html__( 'Error', 'give' ), array( 'response' => 403 ) );
 	}
@@ -16,42 +17,48 @@ function give_process_iats_payment( $donation_data ) {
 
 	// Get agent credentials.
 	$agent_credential = give_iats_get_agent_credentials();
-	$agentCode        = $agent_credential['code'];            // Assigned by iATS
-	$password         = $agent_credential['password'];        // Assigned by iATS
+	$agentCode        = $agent_credential['code'];            // Assigned by iATS.
+	$password         = $agent_credential['password'];        // Assigned by iATS.
 
 	// Process link.
 	$iATS_PL = new iATS\ProcessLink( $agentCode, $password, give_iats_get_server_name() );
 
 	$request = array(
-		'creditCardNum'    => $donation_data['post_data']['card_number'],
-		'creditCardExpiry' => $donation_data['post_data']['card_expiry'],
-		'cvv2'             => $donation_data['post_data']['card_cvc'],
-		'firstName'        => $donation_data['post_data']['give_first'],
-		'lastName'         => $donation_data['post_data']['give_last'],
-		'address'          => $donation_data['post_data']['card_address'],
-		'address2'         => $donation_data['post_data']['card_address_2'], // Custom data.
-		'city'             => $donation_data['post_data']['card_city'],
-		'state'            => $donation_data['post_data']['card_state'],
-		'country'          => $donation_data['post_data']['billing_country'],
-		'zipCode'          => $donation_data['post_data']['card_zip'],
-		'total'            => $donation_data['post_data']['give-amount'],
-		'comment'          => 'givewp',
-		'currency'         => give_get_currency(),
-		'mop'              => give_iats_get_card_name_by_type( $card['type'] ),
+		'creditCardNum'     => $donation_data['card_info']['card_number'],
+		'creditCardExpiry'  => give_iats_format_expiration_date( $donation_data ),
+		'cvv2'              => $donation_data['card_info']['card_cvc'],
+		'firstName'         => $donation_data['post_data']['give_first'],
+		'lastName'          => $donation_data['post_data']['give_last'],
+		'address'           => $donation_data['card_info']['card_address'],
+		'address2'          => $donation_data['card_info']['card_address_2'], // Custom data.
+		'city'              => $donation_data['card_info']['card_city'],
+		'state'             => $donation_data['post_data']['card_state'],
+		'country'           => $donation_data['post_data']['billing_country'],
+		'zipCode'           => $donation_data['post_data']['card_zip'],
+		'total'             => $donation_data['post_data']['give-amount'],
+		'comment'           => 'givewp',
+		'currency'          => give_get_currency(),
+		'mop'               => give_iats_get_card_name_by_type( $card['type'] ),
+		'customerIPAddress' => give_get_ip(),
 	);
 
 	// Make the API call using the ProcessLink service.
 	$response = $iATS_PL->processCreditCard( $request );
 
-	// Verify successful call
-	if ( ! isset( $response['AUTHORIZATIONRESULT'] ) || 'OK' != substr( trim( $response['AUTHORIZATIONRESULT'] ), 0, 2 ) ) {
-		give_record_gateway_error( __( ' Error', 'give-iats' ), sprintf( __( 'There was an error processing your payment (iATS\'s API was down). Error: %s', 'give-iats' ), json_encode( $response ) ), 0 );
+	// Verify successful call. If not successful, display error.
+	if ( ! isset( $response['AUTHORIZATIONRESULT'] ) || 'OK' !== substr( trim( $response['AUTHORIZATIONRESULT'] ), 0, 2 ) ) {
 
-		// Redirect to donation form.
+		give_record_gateway_error( __( ' Error', 'give-iats' ), sprintf( __( 'There was an error processing your donation payment. Error: %s', 'give-iats' ), json_encode( $response ) ), 0 );
+
+		give_iats_set_error( $response );
+
+		// Redirect back to donation form.
 		give_send_back_to_checkout( array(
 			'payment-mode'      => $donation_data['post_data']['give-gateway'],
-			'give-iats-message' => $response['code'],
+			'give-iats-message' => isset( $response['code'] ) ? $response['code'] : '',
 		) );
+
+		return false;
 	}
 
 	$form_id  = intval( $donation_data['post_data']['give-form-id'] );
@@ -145,6 +152,7 @@ add_action( 'give_checkout_error_checks', 'give_iats_varify_donation_data', 9999
  * @return bool
  */
 function give_iats_donation_refund( $do_change, $donation_id, $new_status, $old_status ) {
+
 	$donation = new Give_Payment( $donation_id );
 
 	// Bailout.
@@ -163,14 +171,15 @@ function give_iats_donation_refund( $do_change, $donation_id, $new_status, $old_
 	$request = array(
 		'transactionId' => give_get_payment_transaction_id( $donation->ID ),
 		'total'         => - $donation->total,
-		'comment'       => sprintf( __( "Refund for donation %d", 'give-iats' ), $donation->ID ),
+		'comment'       => sprintf( __( 'Refund for donation %d', 'give-iats' ), $donation->ID ),
 	);
 
 	// Make the API call using the ProcessLink service.
 	$response = $iATS_PL->processCreditCardRefundWithTransactionId( $request );
 
-	// Verify successful call
-	if ( 'OK' != substr( trim( $response['AUTHORIZATIONRESULT'] ), 0, 2 ) ) {
+	// Verify successful API call. If not successful log error.
+	if ( isset( $response['code'] ) && '0' !== $response['code'] ) {
+
 		$url_data = parse_url( $_SERVER['REQUEST_URI'] );
 
 		// Build query
@@ -182,6 +191,7 @@ function give_iats_donation_refund( $do_change, $donation_id, $new_status, $old_
 		$url = home_url( "/{$url_data['path']}?" . http_build_query( $url_query ) );
 
 		// Redirect.
+
 		wp_safe_redirect( $url );
 		exit();
 	}
@@ -214,16 +224,53 @@ function give_iats_show_refund_transaction_id( $donation_id ) {
 		return;
 	}
 
-	if ( $refund_id = give_get_payment_meta( $donation_id, '_give_payment_refund_id', true ) ):
+	if ( $refund_id = give_get_payment_meta( $donation_id, '_give_payment_refund_id', true ) ) :
 		?>
-		<div class="give-admin-box-inside">
-			<p>
-				<strong><?php esc_html_e( 'Refund ID:', 'give' ); ?></strong>&nbsp;
+        <div class="give-admin-box-inside">
+            <p>
+                <strong><?php esc_html_e( 'Refund ID:', 'give' ); ?></strong>&nbsp;
 				<?php echo $refund_id; ?>
-			</p>
-		</div>
+            </p>
+        </div>
 		<?php
 	endif;
 }
 
 add_action( 'give_view_order_details_payment_meta_after', 'give_iats_show_refund_transaction_id' );
+
+/**
+ * Display error to the user according to the error codes from iATS.
+ *
+ * @see http://home.iatspayments.com/developer-info/reject-codes/#top
+ *
+ * @param $api_response
+ */
+function give_iats_set_error( $api_response ) {
+
+	$rejection_code   = isset( $api_response['code'] ) ? $api_response['code'] : '';
+	$response_message = isset( $api_response['message'] ) ? $api_response['message'] : '';
+
+	switch ( true ) {
+		case in_array( $rejection_code, array( '40' ) ):
+			$message = __( 'The card number provided is not valid. Please try your donation again with a valid card number.', 'give-iats' );
+			break;
+		case in_array( $rejection_code, array( '19' ) ):
+			$message = __( 'The CVV2 code security code is incorrect. Please try your donation again with a valid CVV2 number.', 'give-iats' );
+			break;
+		case in_array( $rejection_code, array( '4' ) ):
+			$message = __( 'The card expiration date is incorrect. Please try your donation again with a valid card expiration date.', 'give-iats' );
+			break;
+		case in_array( $rejection_code, array( '23', 'TIMEOUT', '22' ) ):
+			$message = __( 'The payment gateway is having trouble processing donations at the moment. Please try again later.', 'give-iats' );
+			break;
+		case in_array( $rejection_code, array( '18', '2', '7', '8', '9', '10', '12', '14' ) ):
+			$message = $response_message;
+			break;
+		default:
+			$message = __( 'An error occurred while processing the donation. Please try again.', 'give-iats' );
+	}
+
+
+	give_set_error( 'iats_error', $message );
+
+}
